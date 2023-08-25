f6d9250590dd94af1c9f1e53aeafc9abb1d22c44
Bradley Chung <rcn6795@autuni.ac.nz>
2023-08-26 00:13:14 +1200

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -9,3 +9,12 @@ DB = os.environ["DB"]
 DB_HOST = os.environ["DB_HOST"]
 
 app = Flask(__name__)
+
+def init_tables():
+    CREATE_TABLE = "CREATE TABLE IF NOT EXIST CATEGORIES (categoryid INT NOT NULL, categoryname TEXT NOT NULL, categoryimage TEXT)"
+    conn = psycopg2.connect(host=DB_HOST, database=DB, user=DB_USER, passowrd=DB_PASSWORD)
+    cursor = conn.cursor()
+    cursor.execute(CREATE_TABLE)
+    conn.commit()
+    cursor.close()
+    conn.close()
